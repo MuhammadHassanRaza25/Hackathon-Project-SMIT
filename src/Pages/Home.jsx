eb299dd550555df1f71241131755daa6d67660ca
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2025-01-24 15:13:15 +0500

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home(){
+
+    return(
+        <>
+          <h1 className="text-xl text-green-500 drop-shadow-xl text-center mt-10">Home Page</h1>
+        </>
+    )
+}
+
+export default Home;
